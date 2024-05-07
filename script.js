2b055c4d7096845ef16d3cdf4eca9a3f7ac4dae9
Devansh <bajpaidevansh4@gmail.com>
2024-05-07 20:15:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let modeBtn = document.querySelector("#main-btn");
+let main = document.querySelector("#main");
+const root = document.documentElement;
+
+let isDark = false;
+
+modeBtn.addEventListener("click", () => {
+    if (!(isDark)) {
+        modeBtn.style.color = "white";
+        modeBtn.style.backgroundColor = "black";
+        main.style.backgroundColor = "black";
+        root.style.setProperty('--main-color', "white");
+        isDark = true;
+    }
+    else {
+        modeBtn.style.color = "black";
+        modeBtn.style.backgroundColor = "white";
+        main.style.backgroundColor = "rgb(227, 228, 212)";
+        root.style.setProperty('--main-color', "black");
+        isDark = false;
+    }
+})
+
